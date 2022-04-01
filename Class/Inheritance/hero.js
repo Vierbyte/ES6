bc1cc52c28628362c7b15e3de8e74c4bb26b22e6
Corey Lindsey <corey.lindsey.r@gmail.com>
2022-03-31 20:19:06 -0400

    genin if else if added, corrected chunin else if by replacing || with &&.

--- Class/Inheritance/hero.js
+++ Class/Inheritance/hero.js
@@ -25,25 +25,38 @@ class Ninja {
     if(strength >= 9000) {
         this.rank = "Hokage"
     }
-    else if(strength < 9000 || strength >= 7000) {
+    else if(strength < 9000 && strength >= 7000) {
         this.rank = "Jonin"
+    }
+    else if(strength < 7000 && strength >= 4000) {
+        this.rank = "Chunin"
+    }
+    else if(strength < 4000) {
+        this.rank = "Genin"
     }
 
 }
-}
+};
 
 class Hokage extends Ninja {
     constructor(realName, alias, location, speed, strength) {
         super(realName, alias, location, speed, strength);
     }
-}
+    chakraBoost() {
+        this.powerLevel = this.powerLevel * 50
+    }
+};
 
-forthHokage = new Hokage("Minato", "The Yellow Flash", "Konoha", 7000, 2500) 
-forthHokage.powers.specialMoves.push("Rasengan", "Flying Raijin", "Sealing Jutsu")
-sixthHokage = new Hokage("Kakashi", "Copy Ninja", "Konoha", 4000, 5000)
-sixthHokage.powers.specialMoves.push("Sharingan", "Chidori", "Kamui")
-// forthHokage.powers.speed = 500;
-// forthHokage.powers.strength = "Country Level"
+konohomaru = new Ninja("Konohomaru", "Lord Third's Grandson", "Konoha", 1000, 500);
+
+forthHokage = new Hokage("Minato", "The Yellow Flash", "Konoha", 7000, 2500); 
+forthHokage.powers.specialMoves.push("Rasengan", "Flying Raijin", "Sealing Jutsu");
+sixthHokage = new Hokage("Kakashi", "Copy Ninja", "Konoha", 4000, 5000);
+sixthHokage.powers.specialMoves.push("Sharingan", "Chidori", "Kamui");
+
+konohomaru.ninjaRank();
+
+console.log(konohomaru);
 console.log(forthHokage);
 forthHokage.canFly();
 console.log(forthHokage);
@@ -51,8 +64,9 @@ forthHokage.ninjaRank();
 console.log(forthHokage);
 forthHokage.ninjaRank();
 console.log(forthHokage);
-console.log(forthHokage.powerLevel)
-console.log(forthHokage.fl)
-console.log(sixthHokage)
+console.log(forthHokage.powerLevel);
+console.log(forthHokage.fl);
+console.log(sixthHokage);
 sixthHokage.ninjaRank();
+sixthHokage.chakraBoost();
 console.log(sixthHokage);
